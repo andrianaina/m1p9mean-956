9f9664ed6bd68d5d909a4f6f5dbcb59620fd9edf
Henintsoa Andrianaina <andrianainahenintsoa@gmail.com>
2022-04-06 16:12:30 +0300

    template client plus login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { ListPlatComponent } from './list-plat/list-plat.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:LoginComponent},
+  {path:'login',component:LoginComponent},
+  {path:'register',component:RegisterComponent},
+  {path:'plat',component:ListPlatComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { PlatComponent } from './plat/plat.component';
+import { ListPlatComponent } from './list-plat/list-plat.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MenuComponent } from './menu/menu.component';
+import { PaginationComponent } from './pagination/pagination.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PlatComponent,
+    ListPlatComponent,
+    NavbarComponent,
+    MenuComponent,
+    PaginationComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
